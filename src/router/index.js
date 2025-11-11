feba61959873701cbf8f8511fd6f69933168d53c
Fiery106 <fiery106@gmail.com>
2025-11-11 13:44:47 +0100

    11.11.25 - Index.js

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Home from "@/views/HomeView.vue"
 import Student from "@/views/StudentView.vue"
 import Company from "@/views/CompanyView.vue"
 import Nav from "@/views/NavView.vue"
+import Login from "@/views/LoginView.vue"
+import Portal from "@/views/PortalView.vue"
 import Error from "@/views/ErrorView.vue"
 
 const routes = [
@@ -33,6 +35,18 @@ const routes = [
         path: "/:pathMatch(.*)",
         name: "feil",
         component: Error,
+        alias: "/:pathMatch"
+    },
+    {
+        path: "/:pathMatch(.*)",
+        name: "login",
+        component: Login,
+        alias: "/:pathMatch"
+    },
+    {
+        path: "/:pathMatch(.*)",
+        name: "kursportal",
+        component: Portal,
         alias: "/:pathMatch"
     },
 ]
