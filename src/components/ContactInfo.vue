51638356e9024c1e81d76f1695a87f8bca832ab2
Fiery106 <fiery106@gmail.com>
2026-03-05 13:20:09 +0100

    05.03.26 - Business Kort 3
    - Noen små rettelser

--- src/components/ContactInfo.vue
+++ src/components/ContactInfo.vue
@@ -61,74 +61,84 @@ Jeg er for dum til dette T_T
     </div>
 
 
-    <div class="flex flex-col gap-8 gap-x-1 items-center p-8">
+    <div class="flex flex-col gap-8 items-center p-8">
         <h1 class="dark:text-neutral-50 text-3xl md:text-4xl col-span-full text-center">
             Ta Kontakt
         </h1>
 
-        
-        <div class="card">
-            <div class="flex max-3xs:hidden items-center gap-2 ">
-                <Logo class="not-dark:invert h-4" />
+        <div class="flex flex-col lg:grid grid-cols-4 not-md:w-full items-center gap-8">
+            <div v-for="value in 2" class="card">
+                <div class="flex max-3xs:hidden items-center gap-2 ">
+                    <Logo class="not-dark:invert h-4" />
 
-                <h3 class="max-xs:text-lg dark:text-neutral-50">
-                    Utviklerkurs
-                </h3>
-            </div>
+                    <h3 class="max-xs:text-lg dark:text-neutral-50">
+                        Utviklerkurs
+                    </h3>
+                </div>
 
 
-            <div class="flex gap-8 items-center justify-center h-full sm:px-4 @container">
-                <KeepAlive>
-                    <img :src="Anna" class="hidden select-none @min-sm:block size-32 object-center object-cover rounded-full"/>
-                </KeepAlive>
+                <div class="flex gap-8 items-center justify-center h-full @min-sm:p-4">
+                    <KeepAlive v-if="true">
+                        <img :src="Anna" class="hidden select-none @min-sm:block size-32 object-center object-cover rounded-full"/>
+                    </KeepAlive>
 
-                
-                <div class="@max-sm:text-center flex flex-col justify-between min-xs:gap-8 gap-4 sm:gap-1 w-full">
-                    <div class="flex flex-col gap-1">
-                        <h3 class="min-sm:text-lg border-b-2 font-bold border-neutral-500">
-                            Anna Garmann Theodorsen
-                        </h3>
-
-                        <p class="emphasis">
-                            Fagleder AFT
-                        </p>
+                    <div v-else class="justify-center items-center bg-alf-blue dark:bg-cyan-800 hidden select-none @min-sm:flex size-32 rounded-full shrink-0">
+                        <Icon class="text-7xl" />
                     </div>
-                    
 
-                    <div class="flex @sm:flex-col justify-around gap-4 @sm:gap-1">
-                        <!-- <div v-for="value in 2"></div> -->
 
-                        <div class="flex items-center gap-2 w-full">
-                            <Icon :id="5" />
+                    <div class="@max-sm:text-center flex flex-col justify-between min-xs:gap-8 gap-4 @min-sm:gap-1 w-full">
+                        <div class="flex flex-col gap-1">
+                            <h3 class="text-lg @min-3xs:text-xl border-b-2 font-bold border-neutral-500">
+                                Anna Garmann Theodorsen
+                            </h3>
 
-                            <a href="" target="_blank" class="w-full not-sm:no-underline line-clamp-1 bg-neutral-200 hover:bg-neutral-300 dark:bg-neutral-700 dark:hover:bg-neutral-600 rounded-full p-1 px-3">
-                                <p class="block @min-sm:hidden">
-                                    Email
-                                </p>
+                            <p class="emphasis">
+                                Fagleder AFT
+                            </p>
+                        </div>
+                        
 
-                                <p class="hidden @min-sm:block">
-                                    anna@alf.no
-                                </p>
-                            </a>
+                        <div v-if="true" class="flex @sm:flex-col justify-around gap-4 @sm:gap-1">
+                            <!-- <div v-for="value in 2"></div> -->
+
+                            <div class="flex items-center gap-2 w-full">
+                                <Icon :id="5" />
+
+                                <a href="" target="_blank" class="w-full not-sm:no-underline line-clamp-1 bg-neutral-200 hover:bg-neutral-300 dark:bg-neutral-700 dark:hover:bg-neutral-600 rounded-full p-1 px-3">
+                                    <p class="block @min-sm:hidden">
+                                        Email
+                                    </p>
+
+                                    <p class="hidden @min-sm:block">
+                                        anna@alf.no
+                                    </p>
+                                </a>
+                            </div>
+
+                            <div class="flex items-center gap-2 w-full">
+                                <Icon :id="4" />
+
+                                <a href="" target="_blank" class="w-full not-sm:no-underline line-clamp-1 bg-neutral-200 hover:bg-neutral-300 dark:bg-neutral-700 dark:hover:bg-neutral-600 rounded-full p-1 px-3">
+                                    <p class="block @min-sm:hidden">
+                                        Ring 
+                                    </p>
+
+                                    <p class="hidden @min-sm:block">
+                                        +47 91 56 85 51
+                                    </p>
+                                </a>
+                            </div>
                         </div>
 
-                        <div class="flex items-center gap-2 w-full">
-                            <Icon :id="4" />
+                        <div v-else>
 
-                            <a href="" target="_blank" class="w-full not-sm:no-underline line-clamp-1 bg-neutral-200 hover:bg-neutral-300 dark:bg-neutral-700 dark:hover:bg-neutral-600 rounded-full p-1 px-3">
-                                <p class="block @min-sm:hidden">
-                                    Ring 
-                                </p>
-
-                                <p class="hidden @min-sm:block">
-                                    +47 91 56 85 51
-                                </p>
-                            </a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
 
         <div class="flex not-md:flex-col items-center gap-4">
             <ContactInfoContent class="dark:text-neutral-50 w-full" />
