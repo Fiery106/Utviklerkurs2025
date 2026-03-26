ad8aa1db1cdd20f71d88109f06f8ba78b8639b88
Fiery106 <fiery106@gmail.com>
2026-03-26 11:55:09 +0100

    Update Navigation.vue

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -30,9 +30,9 @@ defineProps({
 
         <div v-else>
             <nav v-for="page in main_pages" :key="page.id" class="min-h-fit lg:h-48 xl:h-56">
-                <img :src="page.img" class="hidden lg:block object-cover object-center size-full flex-1 shrink-0" />
+                <img :src="page.img" class="hidden lg:block object-cover object-center size-full basis-1/3" />
 
-                <div :class="`${page.block_color} block-nav not-lg:-mx-8`">
+                <div :class="`${page.block_color} block-nav not-lg:-mx-8 basis-2/3`">
                     <div class="flex flex-col gap-4 p-8">
                         <Quote :message="page.message" :title="page.title">
                             <Icon :id="page.icon_id" class="text-2xl" />
