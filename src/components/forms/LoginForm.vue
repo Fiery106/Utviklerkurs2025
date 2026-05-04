e3432284ec9fd340f1f21c77fcea68c4ed37d3d5
Fiery106 <fiery106@gmail.com>
2026-05-04 13:39:54 +0200

    04.05.26 - Innloggingsform

--- src/components/forms/LoginForm.vue
+++ src/components/forms/LoginForm.vue
@@ -1,37 +1,54 @@
 <script setup>
+import { ref } from 'vue';
 import Profile from '@/components/blocking/Profile.vue';
+import Card from '@/components/blocking/Card.vue';
+
+const active = ref(false)
 </script>
 
 
 <template>
-    <div class="flex items-center gap-8 container">
-        <Profile />
-        
-        <form id="login" action="huskåbytteutsiden.com" method="post" class="flex flex-col gap-2 w-full">
-            <input required type="email" name="epost" placeholder="E-post" />
+    <form id="login" action="huskåbytteutsiden.com" method="post" class="flex flex-col gap-4">
+        <Card class="gap-8">
+            <div class="flex items-center gap-8 container">
+                <Profile />
+                
+                <div class="flex flex-col gap-2 w-full">
+                    <input required type="email" name="epost" placeholder="E-post" autofocus />
 
-            <div class="h-0.5 bg-neutral-500"></div>
+                    <div class="h-0.5 bg-neutral-500"></div>
 
-            <input required type="text" name="passord" placeholder="Passord" />
+                    <input required type="text" name="passord" placeholder="Passord" />
 
-            <div class="flex not-md:flex-col md:items-center justify-between gap-4">
-                <div class="flex flex-col gap-2">
-                    <a class="select-none">
-                        Glemt passord?
-                    </a>
+                    <div class="flex not-md:flex-col md:items-center justify-between gap-4">
+                        <a class="select-none">
+                            Glemt passord?
+                        </a>
 
-                    <label class="flex items-center gap-2 select-none">
-                        <p>
-                            Husk meg
-                        </p>
-                        <input type="checkbox" class="size-4" />
-                    </label>
+                        <label for="huskmeg" class="flex items-center gap-2 select-none">
+                            <p>
+                                Husk meg
+                            </p>
+                            <input type="checkbox" id="huskmeg" class="size-4" />
+                        </label>
+                    </div>
                 </div>
-
-                <button type="submit" class="button-black select-none basic-button not-md:w-full justify-center">
-                    Logg på ->
-                </button>
             </div>
-        </form>
-    </div>
+        </Card>
+
+
+        <div class="flex justify-between">
+            <router-link to="/" class="basic-button button-black">
+                Gå tilbake
+            </router-link>
+
+            <button v-if="active" type="submit" @click.prevent="console.log('hello world')" class="button-black select-none basic-button not-md:w-full justify-center">
+                Logg på ->
+            </button>
+
+            <router-link v-if="!active" to="/kursportal" class="basic-button button-black"> <!-- Skal flyttes senere -->
+                Logg på ->
+            </router-link>
+        </div>
+    </form>
 </template>
--- src/views/InnloggingView.vue
+++ src/views/InnloggingView.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { ref } from 'vue';
-import Card from '@/components/blocking/Card.vue';
 import LoginForm from '@/components/forms/LoginForm.vue';
 
 const warning = ref(false)
@@ -9,32 +8,15 @@ const tekst = "mangler passord & navn"
 
 
 <template>
-    <div class="flex flex-col items-center justify-center h-screen gap-8">
-        <div class="flex flex-col gap-4">
-            <h1 class="wall-text text-center">
-                Kursportalen
-            </h1>
+    <div class="flex flex-col items-center justify-center h-screen gap-4">
+        <h1 class="wall-text text-center">
+            Kursportalen
+        </h1>
 
-            <h2 v-if="warning" name="varsel" class="w-full text-center animate-pulse bg-gradient-to-r via-rose-500 text-neutral-50">
-                {{ tekst.toUpperCase() }}
-            </h2>
+        <h2 v-if="warning" name="varsel" class="w-full text-center animate-pulse bg-gradient-to-r via-rose-500 text-neutral-50">
+            {{ tekst.toUpperCase() }}
+        </h2>
 
-            
-
-            <Card class="gap-8">
-                <LoginForm />
-            </Card>
-
-            <div class="flex justify-between">
-                <router-link to="/" class="basic-button button-black"> <!-- Skal flyttes senere -->
-                    Gå tilbake
-                </router-link>
-
-                <router-link to="/kursportal" class="basic-button button-black"> <!-- Skal flyttes senere -->
-                    Logg inn ->
-                </router-link>
-            </div>
-            
-        </div>
+        <LoginForm />
     </div> 
 </template>
